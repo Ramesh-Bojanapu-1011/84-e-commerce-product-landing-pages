9bb468bff29a936e0d468b2a8ec9e0fbccb0c17a
Ramesh Bojanapu <84425972+Ramesh-Bojanapu-1011@users.noreply.github.com>
2025-10-30 23:05:35 +0530

    fix: downgrade Next.js version to 15.5.4 and adjust build scripts for compatibility

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
+
   reactStrictMode: true,
   devIndicators: false,
   images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
